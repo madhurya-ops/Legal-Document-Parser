e45577cb69098ae226c3105c01130ed8de7137c3
Madhurya Mishra <65159266+madhurya-ops@users.noreply.github.com>
2025-06-20 12:10:20 +0530

    Update client.py, handled merge conflict

--- llm/client.py
+++ llm/client.py
@@ -1,25 +1,3 @@
-<<<<<<< HEAD:llm/client.py
-import requests
-import os
-from app.core.config import SYSTEM_PROMPT
-HF_API_URL = "https://router.huggingface.co/featherless-ai/v1/chat/completions"
-
-headers = {
-    "Authorization": os.getenv("HF_API_KEY"),
-    "Content-Type": "application/json"
-}
-def query_llm(prompt: str, context: str = SYSTEM_PROMPT) -> str:
-    data = {
-        "model": "mistralai/Mistral-7B-Instruct-v0.2",
-        "messages": [
-            {"role": "system", "content": context},
-            {"role": "user", "content": prompt}
-        ]
-    }
-    response = requests.post(HF_API_URL, headers=headers, json=data)
-    response.raise_for_status()
-    return response.json()["choices"][0]["message"]["content"]
-=======
 import requests
 import os
 
@@ -42,4 +20,3 @@ def query(context: str, prompt: str) -> str:
     print(response.text)  
     response.raise_for_status()  
     return response.json()["choices"][0]["message"]["content"]
->>>>>>> 281b2fccb4279bdff35c5c9e547e54f791c14688:llm.py
